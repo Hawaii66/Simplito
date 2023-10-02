1c1093d3f9d091b4aa54207a7bd7a98387fc7802
Sebastian Ahlman <hawaiilive@outlook.com>
2023-10-02 10:24:22 +0200

    init

---
+++ simplito/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      <div>
+        <h1>Simplito UF</h1>
+        <p>Creating small easy to use products</p>
+      </div>
+    </main>
+  );
+}
