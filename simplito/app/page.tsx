6c1c7799941c12c93dc0af9571b1d6413ccb25b9
Sebastian <hawaiilive@outlook.com>
2023-11-14 22:08:39 +0100

    feat: new website

--- simplito/app/page.tsx
+++ simplito/app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
-      <div>
-        <h1>Simplito UF</h1>
-        <p>Creating small easy to use products</p>
+    <main
+      className="flex flex-1 bg-red-50 w-screen h-screen justify-center items-center"
+      style={{
+        backgroundColor: "#002C2B",
+      }}
+    >
+      <div className="w-2/3 lg:w-1/4 aspect-square">
+        <img src="/logo.png" className="w-full" />
       </div>
     </main>
   );
